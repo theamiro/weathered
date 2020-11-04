2a103609e97c149348186109e39afaea34b91b6c
Michael Amiro <michael.amiro@qhala.com>
2020-11-04 17:55:34 +0300

    Initial Commit

---
+++ Weathered/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weathered
+//
+//  Created by Michael Amiro on 04/11/2020.
+//  Copyright © 2020 Michael Amiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WeatheredTests/WeatheredTests.swift
@@ -0,0 +1,34 @@
+//
+//  WeatheredTests.swift
+//  WeatheredTests
+//
+//  Created by Michael Amiro on 04/11/2020.
+//  Copyright © 2020 Michael Amiro. All rights reserved.
+//
+
+import XCTest
+@testable import Weathered
+
+class WeatheredTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
